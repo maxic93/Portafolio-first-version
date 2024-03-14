c42a71167fe8cd93a438daef4abb892dce4b39d6
Maxi <maxicabral36@gmail.com>
2024-03-14 08:42:27 -0300

    cambio de imagenes de tecnologias

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,11 +1,11 @@
 export const images = [
     'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
-    'https://static-00.iconduck.com/assets.00/next-js-icon-512x512-zuauazrk.png',
+    'https://www.datocms-assets.com/98835/1684410508-image-7.png',
     'https://cdn-icons-png.flaticon.com/256/919/919832.png',
     'https://cdn-icons-png.flaticon.com/512/919/919825.png',
     'https://w7.pngwing.com/pngs/384/848/png-transparent-mysql-php-database-javascript-ajax-carnifex-blue-text-logo-thumbnail.png',
     'https://w7.pngwing.com/pngs/429/921/png-transparent-mongodb-plain-wordmark-logo-icon.png',
     'https://camo.githubusercontent.com/fe67f74ce6e3d4972c26ef3be04332a6bb541259c2aec61aaa1fcb04b1803d23/68747470733a2f2f7061676570726f2e636f2f626c6f672f77702d636f6e74656e742f75706c6f6164732f323032302f30332f72656163742d6e61746976652d6c6f676f2d333234783337352e706e67',
-    'https://static-00.iconduck.com/assets.00/electron-icon-512x512-yg13c2qt.png',
+    'https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png',
     'https://static-00.iconduck.com/assets.00/aws-icon-2048x2048-ptyrjxdo.png'
 ]
